dd88663af67872d60b317f2f8efc1e8f37c43c17
shreyapandit010120-svg <shreyapandit010120@gmail.com>
2026-03-24 17:37:07 +0000

    date

---
+++ .devcontainer/01_basics/mydate.js
@@ -0,0 +1,18 @@
+// let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.getDate());
+// console.log(typeof(mydate))
+// let mycreateddate = new Date("2024-12-2")
+// console.log(mycreateddate.toLocaleString())
+// let mydate = new Date("2024-12-1")
+// let myTimestamp = Date.now();
+// console.log(mydate.getTime())
+// console.log(myTimestamp)
+// console.log(Math.floor(mydate.getTime()))
+let date = new Date();
+console.log(date)
+console.log(date.getMonth()+1)
+console.log(date.getDay())
+ console.log(date.toLocaleString('default',{
+weekday:"long"
+}))
