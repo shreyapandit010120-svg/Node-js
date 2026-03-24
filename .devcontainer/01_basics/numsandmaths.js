96772882f68b26bb19e749ccbc69ec0251aa9303
shreyapandit010120-svg <shreyapandit010120@gmail.com>
2026-03-24 14:45:37 +0000

    math and num

---
+++ .devcontainer/01_basics/numsandmaths.js
@@ -0,0 +1,22 @@
+// const balance= new Number(100)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// const otherNumber = 123.678
+// console.log(otherNumber.toPrecision(2))
+// console.log(otherNumber.toPrecision(3))
+// const value = 1000000000;
+// console.log(value.toLocaleString('en-IN'));
+//Math (function)
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(9.87))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.min(1,4,5,6))
+// console.log(Math.max(2,6,5,4))
+console.log(Math.random())
+console.log(Math.random()*100)
+console.log((Math.random()*10+1))
+const max = 20
+const min = 10
+console.log(Math.random()*(max-min+1)+min)
