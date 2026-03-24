1af2d7eb31cd8e60d99394169c2ed15e2178f8c2
shreyapandit010120-svg <shreyapandit010120@gmail.com>
2026-03-24 12:06:38 +0000

    changes

---
+++ .devcontainer/01_basics/memory.js
@@ -0,0 +1,19 @@
+// let name = "shreya";
+// let name2 = name;
+// name2 = "Shriya;"
+// console.log(name)
+// console.log(name2)
+// let username1 = {
+//     email:"Shreya@gmail",
+//     name:"shreya",
+//     rolno :22
+// }
+// let username2 = username1;
+// username1.email = "shreyaSharma";
+// console.log(username1)
+// console.log(username2)
+// back tick
+const name = "shreya"
+const repocount = 50;
+console.log(`My name is ${name} and my repo count is ${repocount}`)
+
---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,22 @@
+const gameName = new String("Shreya");
+// console.log(gameName[0])
+// console.log(gameName.toUpperCase())
+// console.log(gameName.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+const anotherName2 = gameName.substring(0,4)
+const anotherName1 = gameName.slice(-8,4);
+const name = new String("   Ritika   ");
+// console.log(anotherName1);
+// console.log(anotherName2);
+console.log(name);
+console.log(name.trim());
+console.log(name.replace('i','t'))
+console.log(name.includes('Ritik'))
+const name2 = "R-as-hi"
+console.log(name2.split('-'))
+
+
