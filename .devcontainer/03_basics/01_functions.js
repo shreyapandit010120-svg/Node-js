bddc609e6462a0d399b003fd4099d09fde15ea1b
shreyapandit010120-svg <shreyapandit010120@gmail.com>
2026-03-29 12:31:40 +0000

    changes

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,51 @@
+// function sayMyname(){
+//     console.log("Shreya");
+//     console.log("Shreya");
+// console.log("Shreya");
+// }
+// sayMyname();
+// function twoNumber(number1,number2){
+//   console.log( number1+number2);
+// }
+// twoNumber(2,3)
+// function twoNumber(number1,number2){
+//     // let result = number1+number2;
+//     return number1+number2;
+// }
+//  const result =twoNumber(2,3);
+// console.log("result",result);
+// function cutie(username){
+//     return `this is ${username} and she is the best bachha in the world`;
+
+// }
+// console.log(cutie("shreya"));
+// function login(username){
+//     if(username == undefined){ //!username
+//         return 
+//     }
+//    return `${username} should be defined`
+// }
+
+// console.log(login("shreya"))
+// spread operator
+// function calculatecartValues( val1,val2,...num){
+//     return num;
+
+// }
+// console.log(calculatecartValues(800,900,789,678));
+// const user ={
+//     email:"shreyapandit0121",
+//     rollNo:24,
+//     course:"btech"
+// }
+// function student(anyobject){
+// return `${anyobject.rollNo} and ${anyobject.course}`;
+// }
+// console.log(student(user));
+const newarr = [12,23,34,2,3];
+function returnSecondValue( getArray){
+    return getArray[1];
+
+}
+console.log(newarr[1]);
+
